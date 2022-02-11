a0c64c17f87adadf25bcd28613fa7c8d0b6fa7b4
Ety-Biswangri <etybgc02@gmail.com>
2022-02-11 13:09:33 +0600

    deposit work is completed

--- js/banking.js
+++ js/banking.js
@@ -1,11 +1,25 @@
 document.getElementById('deposit-button').addEventListener('click', function () {
+    // get deposit input
     const depositInput = document.getElementById('deposit-input');
-    const depositAmount = depositInput.value;
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText);
+
 
     // get current deposit
     const depositTotal = document.getElementById('deposit-total');
     const depositTotalText = depositTotal.innerText;
-    depositTotal.innerText = depositAmount;
+    const previousDepositTotal = parseFloat(depositTotalText);
+
+    depositTotal.innerText = previousDepositTotal + depositAmount;
+
+
+    // update account balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    balanceTotal.innerText = previousBalanceTotal + depositAmount;
+
 
     // clear deposit input field
     depositInput.value = '';
